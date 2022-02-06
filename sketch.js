ddc308daecdc5fa8fbf1ad4d18e480e222a6d897
nishanshetty200823 <96942738+nishanshetty200823@users.noreply.github.com>
2022-02-06 13:06:16 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,59 +1,68 @@
-var sea,ship;
-var seaImg,shipImg;
+var path,boy, leftBoundary,rightBoundary;
+var pathImg,boyImg;
+var i;
 
 function preload(){
-  seaImg = loadImage("sea.png");
-  shipImg1=loadAnimation("ship-1.png","ship-2.png");
-//Choose the correct option by uncommenting the right line to load the animation for ship.
-  //shipImg1 = loadAnimation("ship-1.png","ship-1.png","ship-2.png","ship-1.png");
-  //shipImg1 = loadAnimation("ship-1","ship-1","ship-2","ship-1");
-  //shipImg1 = loadAnimation("1.png","2.png","3.png","4.png");
-
+  pathImg = loadImage("path.png");
+  boyImg = loadAnimation("Runner-1.png","Runner-2.png");
 }
 
 function setup(){
+  
   createCanvas(400,400);
   
+// Moving background
+path=createSprite(200,0);
+path.addImage(pathImg);
+path.velocityY = 4;
+path.scale=1.2;
 
-  // Moving background
-  sea=createSprite(400,200);
-  sea.addImage(seaImg);
-  sea.velocityx=-5;
-  sea.scale=0.3;
-  edges =createEdgeSprites();
-
+//creating boy running
+boy = createSprite(180,340,30,30);
+boy.scale=0.08;
+boy.addAnimation("JakeRunning",boyImg);
   
-  ship = createSprite(130,200,30,30);
-  ship.addAnimation("movingShip",shipImg1);
-  ship.scale =0.25;
-  ship.x =90;
-  ship.y =300;
+
+leftBoundary=createSprite(0,0,100,800);
+leftBoundary.visible =false;
+
+
+rightBoundary=createSprite(410,0,100,800);
+rightBoundary.visible = false;
+
+
+
+
 }
 
 function draw() {
-  background("0");
+  background(0);
+  
 
- sea.velocityy =-3;
- sea.x=-3;
- sea.velocityx=-3;
- sea.velocityx=3;
+  path.velocityY = 5;
+  
+  boy.x = World.mouseX;
+  
+  edges= createEdgeSprites();
+  boy.collide(edges[3]);
+  boy.collide(leftBoundary);
+  boy.collide(rightBoundary);
+  
+  //code to reset the background
 
-  if (sea.x<0){
-    sea.x =sea.width/8;
+  if(path.y > 400 ){
+    path.y = height/2;
   }
-  if(keyDown("left")){
-  ship.velocityX=-5;
-}
-  
-if(keyDown("right")){
-  ship.velocityX=5;
-}
 
-if(keyDown("space")){
- ship.velocityX=0;
-}
+  if(path.y > 400 ){
+   
+  path.y = height/2;
+  }
 
+  if(path.y > 400 ){
+path.y = height/2;}
+
+if(path.y > 400 ){path.y = height/2;}
   
-    
   drawSprites();
 }
